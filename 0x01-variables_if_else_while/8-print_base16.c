42150ee5b0c257949a6968b11a465806e59090cd
Farad078 <faradokorie@gmail.com>
2022-09-11 23:56:35 +0100

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ *Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
+
