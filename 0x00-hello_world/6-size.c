9c1b2fd563f88ed242348b7b7eb11a4e9ef8da2d
Farad078 <faradokorie@gmail.com>
2022-09-12 20:10:20 +0100

    new update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - Entry point
  * Description: Using the main function
  * This program prints the size of data types
- * Return: Always 0 (Success)
+ * Return: 0
  */
 
 int main(void)
@@ -14,7 +14,6 @@ int main(void)
 	long int li;
 	long long lli;
 	float f;
-
 	printf("size of a char: %1d byte(s)\n", sizeof(c));
 	printf("size of an int: %1d byte(s)\n", sizeof(i));
 	printf("size of a long int: %1d byte(s)\n", sizeof(li));
