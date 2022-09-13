251ee2891e0be4861b33178dbc339a6eb34b55ae
Farad078 <faradokorie@gmail.com>
2022-09-13 23:03:45 +0100

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times.
+ *
+ * return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int a = 1;
+
+	while (a < 11)
+	{
+		print_alphabet();
+		a++;
+	}
+}
