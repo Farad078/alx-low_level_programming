35828e57a8c7820576c6f002f5c10f9a7f17a140
Farad078 <faradokorie@gmail.com>
2022-09-13 05:04:46 +0100

    new update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
+ *
+ * Description: A write function containing a declaration,
+ * files, buf, nbyte.
+ *
  * Return: Always 1 (Success)
  */
 
-int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
