552e88e3be3317cdaebae054c35cb458dd9fa2a6
Farad078 <faradokorie@gmail.com>
2022-09-16 05:22:10 +0100

    just multiply 2 digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Check main
+ * @a: An input character
+ * @b: An input character
+ * Description: function returns a value
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+	
+}
