c29f9ff27de809efa1593bb13ec0874e2659111a
Farad078 <faradokorie@gmail.com>
2022-09-16 19:09:09 +0100

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@
  * Return: Always 0 (Success)
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char a;
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		if ((a != 2) || (a != 4))
+		if ((a != '2') && (a != '4'))
 		{
 			_putchar (a);
 		}
