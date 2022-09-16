9eefa5af1ab7ecf6ce235ec1e0f096c1cf71946c
Farad078 <faradokorie@gmail.com>
2022-09-16 18:57:37 +0100

    new update

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if ((a != 2) || (a != 4))
+		{
+			_putchar (a);
+		}
+	}
+	_putchar('\n');
+}
