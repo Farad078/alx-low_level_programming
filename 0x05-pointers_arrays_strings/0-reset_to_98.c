baf9b2715f7b75dfad878d9974205810cbc8c577
Farad078 <faradokorie@gmail.com>
2022-09-19 20:02:19 +0100

    98 Battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - check main
+ * Description: function reset a value
+ * @n: A pointer Argument
+ * return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
