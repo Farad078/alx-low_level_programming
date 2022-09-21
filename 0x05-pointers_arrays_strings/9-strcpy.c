20defcf4a397e04c0bee775c936caa2590f73333
Farad078 <faradokorie@gmail.com>
2022-09-21 01:39:55 +0100

    copy string pointer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,10 +14,11 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	char *opt = dest;
 
 	for (i = 0; *(src + i) != '\0'; i++)
 	{
 		*(dest + i) = *(src + i);
 	}
-	return (dest);
+	return (opt);
 }
