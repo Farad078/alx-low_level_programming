32378a525651b6dc7ff635f9945db680bef22062
Farad078 <faradokorie@gmail.com>
2022-09-26 20:10:14 +0100

    locate string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - check main
+ * Description: a function that locates a character in a string
+ * @s: input pointer to string
+ * @c: input character
+ * Return: returns a pointer to character
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s)
+	{
+		if(*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (s);
+}
