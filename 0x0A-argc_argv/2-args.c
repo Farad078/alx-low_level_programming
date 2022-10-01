e0fe5cc5847f4c4d04eba2770c479214c9d6a46d
Farad078 <faradokorie@gmail.com>
2022-10-01 03:07:30 +0100

    Best argument against democracy

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a main function
+ * Description: a program that prints its name,
+ * followed by a new line
+ * @argc: argument counter
+ * @argv: a pointer argument
+ * Return: 0 always succeed
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
