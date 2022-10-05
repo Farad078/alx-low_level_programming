ed8758191c13d165ca99cd11f6e36c0645aa9682
Farad078 <faradokorie@gmail.com>
2022-10-05 02:06:44 +0100

    allocate new memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - check main
+ * Description: a function that returns a pointer to a
+ * newly allocated space in memory. which contains a copy
+ * of the string given as a parameter.
+ * @str: a pointer to a string
+ * Return: a pointer to a string
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int n = strlen(str);
+	char * t;
+	unsigned int i = 0;
+
+
+	t = malloc(n * sizeof(char));
+
+	for (; i < n; i++)
+	{
+		t[i] = str[i];
+	}
+
+	if (t == NULL)
+		return (NULL);
+	else
+		return (t);
+}
