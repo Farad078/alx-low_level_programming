dda036a28cfbf17cbc5608d0f13c405448a78195
Farad078 <faradokorie@gmail.com>
2022-10-12 03:17:34 +0100

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - check dog
+ * Description: a function that initializes a variable
+ * @d: a pointer to an input of type struct dog
+ * @name: input pointer to a character
+ * @age: input of type float
+ * @owner: input pointer to character
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = *name;
+	(*d).age = age;
+	(*d).owner = *owner;
+}
