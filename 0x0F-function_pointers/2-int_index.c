483e05ca0542904ec5ed6eedb5bd8d817c9687ff
Farad078 <faradokorie@gmail.com>
2022-10-13 19:18:24 +0100

    Task2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - check function_pointers
+ * Description: a function that searches for an integer
+ * @array: a pointer to array
+ * @size: input to integer
+ * @cmp: pointer to function
+ * Return: returns Nothing
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int *temp = array;
+	int i = 0, count = 0;
+	int (*func)(int);
+
+	func = cmp;
+	if ((array == NULL) || (cmp == NULL))
+		return (-1);
+	while (i < size)
+	{
+		if ((*func)(temp[i]) == 1)
+		{
+			count += i;
+			break;
+		}
+		i++;
+	}
+	return (count);
+}
