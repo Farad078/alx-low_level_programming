3192b434153c2d61ba6fbaa52df79097b55c1af3
Farad078 <faradokorie@gmail.com>
2022-10-15 04:22:00 +0100

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i, x, j, k = 0;
 	va_list args;
 
-	if (separator == NULL || n == 0)
+	if (separator == NULL)
 		return;
 	j = n - 1;
 	va_start(args, n);
