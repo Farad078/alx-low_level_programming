125276e46c464f8755dd31686972ee4975561004
Farad078 <faradokorie@gmail.com>
2022-10-15 04:14:13 +0100

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - check variadic_functions
+ * Description: a function that returns the sum of all its parameters.
+ * @separator: pointer to a character
+ * @n: input unsigned integer
+ * Return: return integer
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, x, j, k = 0;
+	va_list args;
+	
+	if (separator == NULL || n == 0)
+		return;
+	j = n - 1;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, unsigned int);
+		printf("%d", x);
+
+		if (k < j)
+		{
+			printf("%s", separator);
+			k++;
+		}
+
+	}
+	printf("\n");
+	va_end(args);
+}
+
+
