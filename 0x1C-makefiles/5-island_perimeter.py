a173ce03797ba3e878d009945ae4f7790343a532
Farad078 <faradokorie@gmail.com>
2023-02-25 15:23:59 +0100

    new update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,16 +11,17 @@ def island_perimeter(grid):
     while m < c:
         j = 0
         for i in grid[m]:
-            if ((i == 1) and (width == 0) and (height == 0)):
-                width += 1
-                height += 1
-                prev_row = j
-                prev_col = m
-            elif ((i == 1) and (width > 0) and (height > 0) and (j > prev_row)):
-                width += 1
-            elif ((i == 1) and (width > 0) and (height > 0) and (m > prev_col)):
-                height += 1
-                prev_col = m
+            if i == 1:
+                if ((width == 0) and (height == 0)):
+                    width += 1
+                    height += 1
+                    prev_row = j
+                    prev_col = m
+                elif ((width > 0) and (height > 0) and (j > prev_row)):
+                    width += 1
+                elif ((width > 0) and (height > 0) and (m > prev_col)):
+                    height += 1
+                    prev_col = m
             j += 1
         m += 1
     return (height + width) * 2
